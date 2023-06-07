dd8bd6938e0c9b978b31dbd00c655c3d57f7f0d3
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:17:42 +0300

    Add files via upload

---
+++ Практическая работа №2/Practice2.2/Practice2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите массу m - ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите высоту h - ");
+int h = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите скорость v - ");
+int v = int.Parse(Console.ReadLine());
+
+
+double e = (m * Math.Pow(v, 2) / 2);
+double p = (m * 9.8 * h);
+
+Console.WriteLine($"Кинетическая энергия тела равна {e}, Потенциальная энергия тела равна {p}");
+
+Console.ReadKey();
---
+++ Практическая работа №2/Practice2/Practice2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double b = double.Parse(Console.ReadLine());
+
+double middlecube = ((a + b) / 2);
+double geometricMean = (Math.Sqrt(a * b));
+
+Console.WriteLine("Среднее арифметическое равно " + middlecube);
+Console.WriteLine("Среднее геометрическое равно " + geometricMean);
+
+Console.ReadKey();
