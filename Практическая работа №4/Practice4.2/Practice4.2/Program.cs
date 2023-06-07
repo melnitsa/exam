81c3fdf48f54506a795b03eb9be4bd4dd3375847
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:26:26 +0300

    Add files via upload

---
+++ Практическая работа №4/Practice4.2/Practice4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+double depositWithInterest = 1000;
+int deposit = 1000;
+bool exit = false;
+
+for (int i = 0; i <= 10; i++)
+{
+    depositWithInterest = depositWithInterest * 0.02 + depositWithInterest;
+
+    if(depositWithInterest - deposit >= 30 && exit != true)
+    {
+        Console.WriteLine("Величина ежемиесячного увеличения вклада привысила 30 рублей через " + i + " месяцев");
+        exit = true;
+    }
+    if(depositWithInterest > 1200)
+    {
+        Console.WriteLine("Размер вклада привысил 1200р через " + i + " месяцев");
+        break;
+    }
+}
+Console.ReadKey();
