36e75e61d68bb647c514676a335d64ede4278063
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:21:20 +0300

    Add files via upload

---
+++ Практическая работа №3/Practice3.1/Practice3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите b: ");
+int b = int.Parse(Console.ReadLine());
+
+int x = 0;
+
+if (a > b)
+{
+    x = 5 * a + b;
+}
+else if (a == b)
+{
+    x = -125;
+}
+else
+{
+    x = (a - 5) / b;
+}
+Console.WriteLine("X = " + x);
+
+
+Console.ReadKey();
---
+++ Практическая работа №3/Practice3.2/Practice3.2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+Console.WriteLine("Введите x: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y: ");
+int y = int.Parse(Console.ReadLine());
+
+if((x >= 0) && (x * x + y * y <= 4 * 4) && (y >= 0))
+{
+    Console.WriteLine("Да,принадлежит");
+}
+else if((x >= 0) && (x * x + y * y <= 5 * 5) && (y <= 0))
+{
+    Console.WriteLine("Да,принадлежит");
+}
+else
+{
+    Console.WriteLine("Нет, не принадлежит");
+}
+
+Console.ReadKey();
