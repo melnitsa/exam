4a7bf3ea01c8846237e396b5856a8a69d185f4b6
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:28:22 +0300

    Add files via upload

---
+++ Практическая работа №5/Practice5.2/Practice5.2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practice5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = 10;
+            int days = 0;
+
+            do
+            {
+                distance = distance * 0.1 + distance;
+                days++;
+
+            }
+            while (distance <= 20);
+
+            Console.WriteLine("На " + days + " день он пробежит больше 20 км.");
+
+            Console.ReadKey();
+        }
+    }
+}
