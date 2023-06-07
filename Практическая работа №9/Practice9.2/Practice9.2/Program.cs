e96adefb1f1515907515a9349bc2f1ff83aabdbf
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:32:42 +0300

    Add files via upload

---
+++ Практическая работа №9/Practice9.2/Practice9.2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practice9._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первою строку: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку: ");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Введите символ: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ch)
+                {
+                    str = str.Insert(i, str1);
+                    i += str1.Length;
+                }
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
