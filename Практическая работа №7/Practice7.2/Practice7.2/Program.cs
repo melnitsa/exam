e99455dbeafbc045a981396e14b10666db28949f
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:29:45 +0300

    Add files via upload

---
+++ Практическая работа №7/Practice7.2/Practice7.2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Practice7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны матрицы: ");
+            int N = int.Parse(Console.ReadLine());
+            int A = 0;
+            int columnCount = 0;
+            int[,] array = new int[N,
+            N];
+            Console.WriteLine("Заполните матрицу");
+            for (int i = 0; 1 < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (array[j, i] != 0)
+                    {
+                        A++;
+                    }
+                    if (A == N)
+                    {
+                        columnCount++;
+                    }
+                }
+                A = 0;
+            }
+            Console.WriteLine(columnCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Практическая работа №7/Practice7.3/Practice7.3/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Practice7._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны матрицы: ");
+            int N = int.Parse(Console.ReadLine());
+            int index = 0;
+            int temp;
+
+            int[,] array = new int[N, N];
+
+            Console.WriteLine("Заполните матрицу");
+            for (int i = 0; 1 < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    array[i, j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int max = array[0, 0];
+
+            for (int i = 1; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+
+                    if (i == j)
+                    {
+                        if (array[i, i] > max)
+                        {
+                            max = array[i, i];
+                            index = i;
+                        }
+                    }
+            }
+
+            Console.WriteLine("Введите номер строки: ");
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                temp = array[T, i];
+                array[T, i] = array[index, i];
+                array[index, i] = temp;
+            }
+
+            Console.WriteLine("\nПреобразованная матрица: ");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
