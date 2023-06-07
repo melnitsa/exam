419dde6482814557227dc82a261f7d44ff8f33c5
melnitsa <127494945+melnitsa@users.noreply.github.com>
2023-06-07 17:30:43 +0300

    Add files via upload

---
+++ Практическая работа №8/Practice8.2/Practice8.2/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Practice8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер матрицы: ");
+            int length = int.Parse(Console.ReadLine());
+
+            int[,] array = new int[length, length];
+
+            Console.WriteLine("Заполните массив: ");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.WriteLine("Введите число: ");
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Массив до сортировки: ");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int lengthOfArrayForSort = 0;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                lengthOfArrayForSort += length - (length - (i + 1));
+            }
+
+            int[] arrayForSort = new int[lengthOfArrayForSort];
+
+            lengthOfArrayForSort = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == j)
+
+                        break;
+                    array[i, j] = arrayForSort[lengthOfArrayForSort];
+                    lengthOfArrayForSort++;
+
+                }
+            }
+
+            Console.WriteLine("Массив после сортировки: ");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
